efc9750740d94f1249639bd55ea883783c50bc67
NashedShahRoni22 <nashed15-11170@diu.edu.bd>
2026-01-11 16:16:59 +0600

    store id is managed from utils

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,9 @@
 import SectionRenderer from "@/components/core/SectionRenderer";
+import { staticStoreId } from "@/utils/storeId";
 
 export default async function Home() {
   const data = await fetch(
-    "https://ecomback.bfinit.com/store/theme/data/695e08b54bf6001a986a4bde",
+    `https://ecomback.bfinit.com/store/theme/data/${staticStoreId}`,
   );
 
   const sections = await data.json();
