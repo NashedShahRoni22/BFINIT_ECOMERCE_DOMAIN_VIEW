7e418a1a095767169e2c611108be5d7b0405c130
NashedShahRoni22 <nashed15-11170@diu.edu.bd>
2026-01-12 14:07:42 +0600

    get url added

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
 import SectionRenderer from "@/components/core/SectionRenderer";
+import { getHost } from "@/utils/get-url";
 import { staticStoreId } from "@/utils/storeId";
 
 export default async function Home() {
+  const host = await getHost();
   const data = await fetch(
     `https://ecomback.bfinit.com/store/theme/data/${staticStoreId}`,
   );
@@ -11,6 +13,7 @@ export default async function Home() {
   return (
     <div>
       <SectionRenderer sections={sections?.data?.sections?.body} />
+      <p>current host: {host}</p>
     </div>
   );
 }
